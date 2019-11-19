53dfdc358460cc82f28da32510f73f6a92784a09
Ferick Andrew <ferickwar@gmail.com>
2019-11-19 16:30:59 +0700

    Capsules schema (wip) #1

---
+++ components/screens/Launches.tsx
@@ -0,0 +1,149 @@
+import React from 'react'
+import
+{
+  Platform,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+  View,
+  Text
+} from 'react-native'
+import { Spinner } from 'native-base'
+import { Query } from 'react-apollo'
+import
+{
+  D_HEIGHT,
+  D_WIDTH
+} from '../../models/dimensions'
+import { GET_PREVIOUS_LAUNCHES } from '../../models/queries/launchesPast'
+
+const Launches = () =>
+{
+  const _renderItem = ({ item }) =>
+  {
+    return (
+      <TouchableOpacity style={styles.itemContainer}>
+        <Text style={styles.itemText}>ID: {item.id}</Text>
+        <Text style={styles.itemText}>Active: {item.active.toString()}</Text>
+        <Text style={styles.itemText}>First Flight: {item.first_flight}</Text>
+        <Text style={styles.itemText}>Company: {item.company}</Text>
+        <Text style={styles.itemText}>Country: {item.country}</Text>
+        {/* <Text style={styles.itemText}>Diameter: {item.diameter}</Text> */}
+        {/* <Text style={styles.itemText}>Engines: {item.engines}</Text> */}
+        <Text style={styles.itemText}>Cost per Launch: {item.cost_per_launch}</Text>
+        <Text style={styles.itemText}>Boosters: {item.boosters}</Text>
+        <Text style={[styles.itemText, item.description ? styles.descriptionText : styles.errorText]}>
+          Description: {item.description}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+  
+  return (
+    <Query query={GET_PREVIOUS_LAUNCHES}>
+      {
+        (res: any) =>
+        {
+          // if(err)
+          //   return (
+          //     <Text style={styles.errorText}>{err}</Text>
+          //   )
+          
+          if(res.loading && !res.data)
+            return (
+              <View style={styles.loadingContainer}>
+                <View style={styles.spinner}>
+                  <Spinner color='blue'/>
+                </View>
+              </View>
+            )
+
+          if(!res.data)
+            return (
+              <View style={styles.loadingContainer}>
+                <View style={styles.spinner}>
+                  <Text>We're having trouble loading the data ...</Text>
+                  <Text>Please try it again later</Text>
+                </View>
+              </View>
+            )
+
+          return (
+            <ScrollView>
+              <FlatList
+                data={res.data}
+                renderItem={(item) => _renderItem(item)}
+              />
+            </ScrollView>
+          )
+        }
+      }
+    </Query>
+  )
+}
+
+const styles = StyleSheet.create(
+{
+  loadingContainer:
+  {
+    flex: 1,
+    width: D_WIDTH
+  },
+  spinner:
+  {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: D_HEIGHT / 2,
+    transform:
+    [
+      { translateY: -D_HEIGHT / 12.5 }
+    ]
+  },
+  container:
+  {
+    flex: 1, //Instead of do 100% of height and width, just use flex: 1,
+  },
+  headerText:
+  {
+    fontSize: 30,
+    marginTop: 30,
+  },
+  itemContainer:
+  {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'gray'
+  },
+  itemText:
+  {
+    fontSize: 20,
+    fontWeight: '500',
+    fontFamily: Platform.select(
+    {
+      ios: 'Chalkboard SE',
+      android: 'sans-serif-condensed'
+    })
+  },
+  errorText:
+  {
+    fontSize: 20,
+    fontWeight: '500',
+    fontFamily: Platform.select(
+    {
+      ios: 'Chalkboard SE',
+      android: 'sans-serif-condensed'
+    }),
+    color: 'red'
+  },
+  descriptionText:
+  {
+    color: 'green',
+  }
+})
+
+export default Launches
